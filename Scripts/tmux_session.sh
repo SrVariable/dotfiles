de6227a708cd75c63ff3d56a23aecb7224586fdc
SrVariable <rojohnibana@gmail.com>
2024-03-15 00:00:04 +0100

    Daily sync Fri Mar 15 12:00:03 AM CET 2024

--- Scripts/tmux_session.sh
+++ Scripts/tmux_session.sh
@@ -8,14 +8,15 @@ for i in "${SESSIONS[@]}"; do
 		tmux new-session -s $i -n "main" -d
 		tmux new-window -t $i: -n "test" -d
 		tmux new-window -t $i: -n "temp" -d
-		tmux send-keys -t $i:test "cd $HOME/Magia/test" C-m
-		tmux send-keys -t $i:test "clear" C-m
+		sleep 0.5
+		tmux send-keys -t $i:test "cd $HOME/Magia/test" C-m; sleep 0.5
+		tmux send-keys -t $i:test "clear" C-m; sleep 0.5
 	fi
 done
 
-tmux send-keys -t ${SESSIONS[0]}:main "cd $HOME/GitRepos/roadmap-retos-programacion/Roadmap" C-m
-tmux send-keys -t ${SESSIONS[0]}:main "clear" C-m
-tmux send-keys -t ${SESSIONS[1]}:main "cd $HOME/GitRepos/push_swap" C-m
-tmux send-keys -t ${SESSIONS[1]}:main "clear" C-m
+tmux send-keys -t ${SESSIONS[0]}:main "cd $HOME/GitRepos/roadmap-retos-programacion/Roadmap" C-m; sleep 0.5
+tmux send-keys -t ${SESSIONS[0]}:main "clear" C-m; sleep 0.5
+tmux send-keys -t ${SESSIONS[1]}:main "cd $HOME/GitRepos/push_swap" C-m; sleep 0.5
+tmux send-keys -t ${SESSIONS[1]}:main "clear" C-m; sleep 0.5
 
 tmux attach -t ${SESSIONS[0]}
