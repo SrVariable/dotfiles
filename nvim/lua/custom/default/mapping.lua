9225b706260d38e6c9c7c0cd96364fb3033d9b0b
SrVariable <rojohnibana@gmail.com>
2025-02-09 00:56:03 +0100

    Daily sync Sun Feb  9 12:56:03 AM CET 2025

--- nvim/lua/custom/default/mapping.lua
+++ nvim/lua/custom/default/mapping.lua
@@ -3,4 +3,4 @@ vim.keymap.set("n", "<C-c>", "<cmd>nohlsearch<CR>", { desc = "Disable highlighte
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("n", "<leader>n", "<cmd>bn<CR>", { desc = "Switch to [N]ext buffer" })
 vim.keymap.set("n", "<leader>p", "<cmd>bp<CR>", { desc = "Switch to [P]revious buffer" })
-vim.keymap.set("n", "<leader>p", "<cmd>bd!<CR>", { desc = "[D]eletes current buffer" })
+vim.keymap.set("n", "<leader>d", "<cmd>bd!<CR>", { desc = "[D]eletes current buffer" })
