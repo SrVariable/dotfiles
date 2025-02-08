6b182eeb56e9e7faa88ad4c6a97e4795c44a1c8a
SrVariable <rojohnibana@gmail.com>
2025-02-09 00:59:03 +0100

    Daily sync Sun Feb  9 12:59:03 AM CET 2025

--- nvim/lua/custom/default/setter.lua
+++ nvim/lua/custom/default/setter.lua
@@ -32,3 +32,15 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.g.have_nerd_font = true
 vim.opt.showmode = false
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = vim.g.user.event,
+	callback = function(args)
+		local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line("$")
+		local not_commit = vim.b[args.buf].filetype ~= "commit"
+
+		if valid_line and not_commit then
+			vim.cmd([[normal! g`"]])
+		end
+	end,
+})
