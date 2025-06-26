75b7374fd29acfa2798415655c1c12464d1ac140
SrVariable <rojohnibana@gmail.com>
2025-06-26 16:00:03 +0200

    Hourly sync Thu Jun 26 04:00:03 PM CEST 2025

--- nvim/lua/custom/default/setter.lua
+++ nvim/lua/custom/default/setter.lua
@@ -60,12 +60,12 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "javascript", "typescript" },
-	callback = function()
-		vim.opt_local.shiftwidth = 2
-		vim.opt_local.tabstop = 2
-		vim.opt_local.softtabstop = 2
-		vim.opt_local.expandtab = true
-	end,
-})
+--vim.api.nvim_create_autocmd("FileType", {
+--	pattern = { "javascript", "typescript" },
+--	callback = function()
+--		vim.opt_local.shiftwidth = 4
+--		vim.opt_local.tabstop = 4
+--		vim.opt_local.softtabstop = 4
+--		vim.opt_local.expandtab = true
+--	end,
+--})
